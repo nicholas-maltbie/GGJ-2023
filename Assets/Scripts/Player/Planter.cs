3d8b9fcb58a2036437b8f93b8c61fbf539702226
Samuel Scherer <swiimii962@gmail.com>
2023-02-05 00:28:25 -0500

    Remove redundant function

--- Assets/Scripts/Player/Planter.cs
+++ Assets/Scripts/Player/Planter.cs
@@ -107,8 +107,6 @@ namespace nickmaltbie.IntoTheRoots.Player
             // Decrease player resources by requested amount
             SpendResourcesForPlant(plant);
 
-            plant.AddRootBonus(connectedTrees.Length);
-
             // If vegetable, multiply points by # of connections.
             int points = plant.plantType == PlantType.Vegetable ? plant.victoryPoints * connectedTrees.Length : plant.victoryPoints;
             PlayerResources.AddVictoryPoints(OwnerClientId, points);
